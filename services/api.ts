9d14f2f9918b1a7770a384205dba7881beee0b1f
Leonardo <leonardo.pacciulli@opah.com.br>
2022-03-15 18:06:28 -0300

    Refresh token with interceptors

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,41 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios';
+import { setCookie, parseCookies } from 'nookies';
+
+let cookies = parseCookies()
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'http://localhost:3333',
+  headers: {
+    Authorization: `Bearer ${cookies['authnextjs.token']}`
+  }
+})
+
+api.interceptors.response.use(response => {
+  return response;
+}, (error: AxiosError) => {
+  if (error.response.status === 401) {
+    if (error.response.data?.code === 'token.expired') {
+      cookies = parseCookies()
+
+      const { 'authnextjs.refreshToken': refreshToken } = cookies;
+      
+      api.post('/refresh', {refreshToken}).then(response => {
+        const { token } = response.data;
+
+        setCookie(undefined, 'authnextjs.token', token, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+        
+        setCookie(undefined, 'authnextjs.refreshToken', response.data.refreshToken, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+
+        api.defaults.headers['Authorization'] = `Bearer ${token}`
+      })
+    } else {
+
+    }
+  }
 })
